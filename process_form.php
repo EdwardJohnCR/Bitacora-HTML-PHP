cde7220275e30dd80c33998d6ecb0a63762019fa
EdwardJohnCR <edwardjohncr@gmail.com>
2024-08-12 13:53:53 -0600

    include logo

--- process_form.php
+++ process_form.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recoger los datos del formulario
     $officerName = htmlspecialchars($_POST['officerName'], ENT_QUOTES, 'UTF-8');
